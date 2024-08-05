2dfe87bf37c1a8982d0d84747541fe343e92bf62
VioletaS <violistancikiene@gmail.com>
2024-08-05 19:44:30 +0300

    Hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,10 +5,14 @@ const imgStyle={
     height:'15rem',
 };
 
+const title=Math.random()>0.5
+? 'Vite Next Generation Frontend Tooling'
+: 'Finito!!!';
+
     return(
         <section>
           <div>
-            <h1>Vite <br />Next Generation <br />Frontend Tooling</h1>
+            <h1>{title}</h1>
             <p>Get ready for a development environment that can <br /> finally catch up with you.</p>
             <div>
               <a href="#">Get Started</a>
