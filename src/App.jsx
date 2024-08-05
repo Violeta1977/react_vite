19d4c6f2ced6af364e817cff8ed47bd62bc05a26
VioletaS <violistancikiene@gmail.com>
2024-08-05 19:56:29 +0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import './App.css';
 import { Features } from './components/features/Features';
+import { Footer } from './components/footer/Footer';
 import { Header } from './components/header/Header';
 import { Hero } from './components/hero/Hero';
-import { Sponsors } from './components/sponsors/Spnsors';
-
+import { Sponsors } from './components/sponsors/Sponsors';
 
 function App() {
   return (
     <>
       <Header />
-      
       <main>
         <Hero />
         <Features />
         <Sponsors />
       </main>
-
-      <footer>
-        <p>Released under the MIT License. (2c4ef113)</p>
-        <p>Copyright &copy; 2019-present Evan You & Vite Contributors</p>
-      </footer>
+      <Footer />
     </>
   );
 }
-
 
 export default App;
