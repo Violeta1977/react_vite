5cc493e66ce99d4779f32ca374d1d21c63c44d75
VioletaS <violistancikiene@gmail.com>
2024-08-05 19:47:48 +0300

    Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Features } from './components/features/Features';
 import { Header } from './components/header/Header';
 import { Hero } from './components/hero/Hero';
 
@@ -7,42 +8,13 @@ function App() {
   return (
     <>
       <Header />
-      <Hero />
-
+      
       <main>
+        <Hero />
+        <Features />
+
         
-        <section>
-          <div>
-            <div>💡</div>
-            <h2>Instant Server Start</h2>
-            <p>On demand file serving over native ESM, no bundling required!</p>
-          </div>
-          <div>
-            <div>⚡️</div>
-            <h2>Lightning Fast HMR</h2>
-            <p>Hot Module Replacement (HMR) that stays fast regardless of app size.</p>
-          </div>
-          <div>
-            <div>🛠️</div>
-            <h2>Rich Features</h2>
-            <p>Out-of-the-box support for TypeScript, JSX, CSS and more.</p>
-          </div>
-          <div>
-            <div>📦</div>
-            <h2>Optimized Build</h2>
-            <p>Pre-configured Rollup build with multi-page and library mode support.</p>
-          </div>
-          <div>
-            <div>🔩</div>
-            <h2>Universal Plugins</h2>
-            <p>Rollup-superset plugin interface shared between dev and build.</p>
-          </div>
-          <div>
-            <div>🔑</div>
-            <h2>Fully Typed APIs</h2>
-            <p>Flexible programmatic APIs with full TypeScript typing.</p>
-          </div>
-        </section>
+        
         <section>
           <a href="#">Sponsor Vite</a>
           <a href="#">Sponser Evan You</a>
