d193d61fbf27aea942c8313afb652bd5e62455a9
VioletaS <violistancikiene@gmail.com>
2024-08-07 09:50:07 +0300

    Hero style

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,10 @@
 import logo from '/vite.svg';
+import './Hero.css';
 
 export function Hero(){
 const imgStyle={
-    height:'15rem',
+    height:'24rem',
+    widht:'100%',
 };
 
 const title=Math.random()>0.5
@@ -10,15 +12,15 @@ const title=Math.random()>0.5
 : 'Finito!!!';
 
     return(
-        <section>
-          <div>
-            <h1>{title}</h1>
-            <p>Get ready for a development environment that can <br /> finally catch up with you.</p>
-            <div>
-              <a href="#">Get Started</a>
-              <a href="#">Why Vite?</a>
-              <a href="#">View on Github</a>
-              <a href="#">⚡️ ViteConf 24!</a>
+        <section className='hero'>
+          <div className='textContent'>
+            <h1 className='title'>{title}</h1>
+            <p className='description'>Get ready for a development environment that can <br /> finally catch up with you.</p>
+            <div className='btnList'>
+              <a className='btn' href="#">Get Started</a>
+              <a className='btn' href="#">Why Vite?</a>
+              <a className='btn' href="#">View on Github</a>
+              <a className='btn' href="#">⚡️ ViteConf 24!</a>
             </div>
           </div>
           <img src={logo} style={imgStyle} alt="Vite img" />
